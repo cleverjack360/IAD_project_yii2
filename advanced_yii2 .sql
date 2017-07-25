d0f871875c4cd69f967ff7c3566906bc56d82403
cleverjack360 <30302200+cleverjack360@users.noreply.github.com>
2017-07-26 03:16:03 +0500

    new database

--- advanced_yii2 .sql
+++ advanced_yii2 .sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 24, 2017 at 06:34 PM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Generation Time: Jul 25, 2017 at 11:33 PM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -38,7 +40,10 @@ CREATE TABLE `companies` (
 --
 
 INSERT INTO `companies` (`company_id`, `company_name`, `company_city`, `company_status`) VALUES
-(1, 'UBITSOFT', 'LOS ANGELES', 'active');
+(1, 'UBITSOFT', 'LOS ANGELES', 'active'),
+(2, 'THQ', 'Atlanta', 'inactive'),
+(3, 'Naughty Dog', 'Los Angeles.', 'active'),
+(4, 'Activision', 'New York', 'active');
 
 -- --------------------------------------------------------
 
@@ -53,6 +58,16 @@ CREATE TABLE `gameinfo` (
   `game_release` date NOT NULL,
   `game_category` enum('action','adventure','RPG','racing','puzzle','fighting','sports') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `gameinfo`
+--
+
+INSERT INTO `gameinfo` (`game_id`, `companies_company_id`, `game_name`, `game_release`, `game_category`) VALUES
+(1, 2, 'Smack Down VS RAW', '2008-08-26', 'fighting'),
+(2, 3, 'Naughty Dog Uncharted', '2012-05-03', 'adventure'),
+(3, 4, 'Call of Duty', '2003-06-14', 'action'),
+(4, 1, 'Watch Dogs', '2014-01-15', 'adventure');
 
 -- --------------------------------------------------------
 
@@ -72,6 +87,30 @@ CREATE TABLE `migration` (
 INSERT INTO `migration` (`version`, `apply_time`) VALUES
 ('m000000_000000_base', 1500490556),
 ('m130524_201442_init', 1500490581);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `id` int(11) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `game_id` int(11) NOT NULL,
+  `review` varchar(500) NOT NULL,
+  `game_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `reviews`
+--
+
+INSERT INTO `reviews` (`id`, `user_name`, `game_id`, `review`, `game_name`) VALUES
+(1, 'Shahzeb Rajput', 3, 'Very nice and precise shooting game.', 'Call of Duty'),
+(2, 'Shahzeb', 4, 'A wonderful hacking and adventure game.', 'Watch Dogs'),
+(3, 'Shahzeb', 2, 'Best version.', 'Naughty Dog'),
+(4, 'Zohaib Najam', 4, 'I gave this game 5 stars.', 'Watch Dogs');
 
 -- --------------------------------------------------------
 
@@ -124,6 +163,13 @@ ALTER TABLE `migration`
   ADD PRIMARY KEY (`version`);
 
 --
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `game_id` (`game_id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -140,12 +186,17 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `companies`
 --
 ALTER TABLE `companies`
-  MODIFY `company_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `company_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `gameinfo`
 --
 ALTER TABLE `gameinfo`
-  MODIFY `game_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `game_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `user`
 --
@@ -160,6 +211,13 @@ ALTER TABLE `user`
 --
 ALTER TABLE `gameinfo`
   ADD CONSTRAINT `gameinfo_ibfk_1` FOREIGN KEY (`companies_company_id`) REFERENCES `companies` (`company_id`);
+
+--
+-- Constraints for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `gameinfo` (`game_id`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
